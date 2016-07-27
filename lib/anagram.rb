719c7c3e2850221daaa7be6a8660ac97729496fc
Andrew Klein <andrew.klein.j@gmail.com>
2016-07-27 17:44:38 -0400

    complete assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  attr_accessor :word
+
+  def match(words)
+    words.select do |test_word|
+      test_word if test_word.split("").sort == self.word.split("").sort
+    end
+  end
+
+end
